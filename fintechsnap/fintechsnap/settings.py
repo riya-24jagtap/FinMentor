95a03a9cf0f3b26347dbd3a30f9c71df56d7df75
Riya <jagtapriya24@gmail.com>
2026-03-01 00:06:00 +0530

    fixed app paths for prodcution

--- fintechsnap/fintechsnap/settings.py
+++ fintechsnap/fintechsnap/settings.py
@@ -25,10 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'analysis',
-    'predictor',
-
-    'financeapp.apps.FinanceappConfig',
+    'fintechsnap.analysis',
+    'fintechsnap.predictor',
+    'fintechsnap.financeapp.apps.FinanceappConfig',
 ]
 
 MIDDLEWARE = [
