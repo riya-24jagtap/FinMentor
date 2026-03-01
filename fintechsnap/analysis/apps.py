5c46d483c4d11d0b3ba7b7777c7362f525118882
Riya <jagtapriya24@gmail.com>
2026-03-02 00:47:38 +0530

    temp: auto create superuser for production

--- fintechsnap/analysis/apps.py
+++ fintechsnap/analysis/apps.py
@@ -4,3 +4,22 @@ from django.apps import AppConfig
 class AnalysisConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'analysis'
+
+from django.contrib.auth import get_user_model
+from django.db.utils import OperationalError
+
+class FinanceappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'financeapp'
+
+    def ready(self):
+        try:
+            User = get_user_model()
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="admin",
+                    email="admin@example.com",
+                    password="admin123"
+                )
+        except OperationalError:
+            pass
