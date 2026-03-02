7c16cb209d4927abb580411cc49048f0c5513ad7
Riya <jagtapriya24@gmail.com>
2026-03-02 20:42:29 +0530

    Fix auth issue by removing DB logic from AppConfig.ready()

--- fintechsnap/analysis/apps.py
+++ fintechsnap/analysis/apps.py
@@ -12,14 +12,3 @@ class FinanceappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'financeapp'
 
-    def ready(self):
-        try:
-            User = get_user_model()
-            if not User.objects.filter(username="admin").exists():
-                User.objects.create_superuser(
-                    username="admin",
-                    email="admin@example.com",
-                    password="admin123"
-                )
-        except OperationalError:
-            pass
