05d25b3b61c967f42ba0104221351c03e6291c21
Riya <jagtapriya24@gmail.com>
2026-03-06 20:24:24 +0530

    Fix production DEBUG handling and deployment settings

--- fintechsnap/fintechsnap/settings.py
+++ fintechsnap/fintechsnap/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # ---------------------------
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'   # change to 'Asia/Kolkata' if you want
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 USE_TZ = True
