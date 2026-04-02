38ead220d89fdf0fed7013eb72e9c1efd49e768d
Riya <jagtapriya24@gmail.com>
2026-04-02 22:10:26 +0530

    Secured database credentials and updated project files

--- fintechsnap/financeapp/urls.py
+++ fintechsnap/financeapp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import AuthenticationForm
 from . import views
 
 urlpatterns = [
